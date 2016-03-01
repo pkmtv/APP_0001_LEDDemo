63cb1233e26507a35c4a6073e9386b8435851826
pkmtv <807276196@qq.com>
2016-03-01 23:39:00 +0800

    v5, called ledOpen, ledCtrl

--- app/src/main/java/com/example/app_0001_leddemo/MainActivity.java
+++ app/src/main/java/com/example/app_0001_leddemo/MainActivity.java
@@ -23,9 +23,6 @@ public class MainActivity extends AppCompatActivity {
     class MyButtonListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-
-            HardControl hardControl = new HardControl();
-
             ledon = !ledon;
             if(ledon) {
                 button.setText("ALL OFF");
@@ -33,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
                 checkBoxLed2.setChecked(true);
                 checkBoxLed3.setChecked(true);
                 checkBoxLed4.setChecked(true);
+
+                for(int i = 0; i < 4; i++)
+                    HardControl.ledCtrl(i, 1);
             }
             else{
                 button.setText("ALL ON");
@@ -40,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
                 checkBoxLed2.setChecked(false);
                 checkBoxLed3.setChecked(false);
                 checkBoxLed4.setChecked(false);
+
+                for(int i = 0; i < 4; i++)
+                    HardControl.ledCtrl(i, 0);
             }
         }
     }
@@ -52,33 +55,41 @@ public class MainActivity extends AppCompatActivity {
             case R.id.LED1:
                 if(checked) {
                     Toast.makeText(getApplicationContext(), "LED1 on", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(0, 1);
                 }
                 else {
                     Toast.makeText(getApplicationContext(), "LED1 off", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(0, 0);
                 }
                 break;
             case R.id.LED2:
                 if(checked) {
                     Toast.makeText(getApplicationContext(), "LED2 on", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(1, 1);
                 }
                 else {
                     Toast.makeText(getApplicationContext(), "LED2 off", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(1, 0);
                 }
                 break;
             case R.id.LED3:
                 if(checked) {
                     Toast.makeText(getApplicationContext(), "LED3 on", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(2, 1);
                 }
                 else {
                     Toast.makeText(getApplicationContext(), "LED3 off", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(2, 0);
                 }
                 break;
             case R.id.LED4:
                 if(checked) {
                     Toast.makeText(getApplicationContext(), "LED4 on", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(3, 1);
                 }
                 else {
                     Toast.makeText(getApplicationContext(), "LED4 off", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(3, 0);
                 }
                 break;
         }
@@ -91,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.BUTTON);
 
+        HardControl.ledOpen();
 
         checkBoxLed1 = (CheckBox) findViewById(R.id.LED1);
         checkBoxLed2 = (CheckBox) findViewById(R.id.LED2);
